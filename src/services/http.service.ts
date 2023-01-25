d4c3e73afff3bebfa749e2929be51d59201e3d69
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2023-01-25 22:06:17 +0400

    feat: Add Axios and Initialized HTTPService

---
+++ src/services/http.service.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+interface IHttpService {
+  get<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+}
+
+const instance: AxiosInstance = axios.create({
+  baseURL: 'https://your-api-url.com/',
+});
+
+const httpService: IHttpService = {
+  get: <T = any>(url: string, config?: AxiosRequestConfig) => instance.get<T>(url, config),
+  post: <T = any>(url: string, data?: any, config?: AxiosRequestConfig) => instance.post<T>(url, data, config),
+  put: <T = any>(url: string, data?: any, config?: AxiosRequestConfig) => instance.put<T>(url, data, config),
+  delete: <T = any>(url: string, config?: AxiosRequestConfig) => instance.delete<T>(url, config),
+};
+
+export default httpService;
