10c34bdc997569572d38de9d7890fff9e20cd834
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2023-01-25 21:25:19 +0400

    feat: Added General Layout to the Project Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './components/searchBar/SearchBar';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col gap-10 md:flex-row w-screen h-screen p-2 overflow-auto bg-main-dark'>
+
+      <Sidebar />
+
+      <div className='grow'>
+
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
+          <div className='p-2 md:col-span-2 md:row-span-1 bg-main-gray rounded-lg'>
+            <SearchBar />
+          </div>
+          <div className='p-4 bg-gray-200 md:col-span-2 md:row-span-1'>Main Temperature</div>
+          <div className='p-4 bg-gray-200 md:col-span-2 md:row-span-1 md:col-start-1'>Hourly Forecast</div>
+          <div className='p-4 bg-gray-200 md:col-span-2 md:row-span-1'>Extra Information</div>
+          <div className='p-4 bg-gray-200 md:col-span-1 md:col-start-3 md:row-span-4 md:row-start-1'>5 Day Forecast</div>
+        </div>
+      </div>
     </div>
   );
 }
